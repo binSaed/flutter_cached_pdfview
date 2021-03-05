2256a7359e4ec66bf4b8a4f08f73e624abcbb404
Abdelrahman Saed <me@abdosaed.ml>
2021-03-06 01:09:38 +0200

    Update pdf_view_wrapper.dart

--- lib/src/pdf_view_wrapper.dart
+++ lib/src/pdf_view_wrapper.dart
@@ -4,37 +4,40 @@ import 'package:flutter_pdfview/flutter_pdfview.dart';
 import '../flutter_cached_pdfview.dart';
 
 class PDFViewWrapper extends StatelessWidget {
-  const PDFViewWrapper({Key? key, required this.pdf, required this.path})
-      : super(key: key);
+  const PDFViewWrapper({
+    Key? key,
+    required this.pdf,
+    required this.path,
+  }) : super(key: key);
 
   /// path for pdf file in phone storage
   final String path;
 
   /// Pdf Model
-  final PDF? pdf;
+  final PDF pdf;
 
   @override
   Widget build(BuildContext context) {
     return PDFView(
       filePath: path,
-      onLinkHandler: pdf!.onLinkHandler,
-      onViewCreated: pdf!.onViewCreated,
-      onRender: pdf!.onRender,
-      onPageChanged: pdf!.onPageChanged,
-      onError: pdf!.onError,
-      onPageError: pdf!.onPageError,
-      gestureRecognizers: pdf!.gestureRecognizers,
-      enableSwipe: pdf!.enableSwipe,
-      swipeHorizontal: pdf!.swipeHorizontal,
-      password: pdf!.password,
-      nightMode: pdf!.nightMode,
-      autoSpacing: pdf!.autoSpacing,
-      pageFling: pdf!.pageFling,
-      pageSnap: pdf!.pageSnap,
-      fitEachPage: pdf!.fitEachPage,
-      defaultPage: pdf!.defaultPage,
-      fitPolicy: pdf!.fitPolicy,
-      preventLinkNavigation: pdf!.preventLinkNavigation,
+      onLinkHandler: pdf.onLinkHandler,
+      onViewCreated: pdf.onViewCreated,
+      onRender: pdf.onRender,
+      onPageChanged: pdf.onPageChanged,
+      onError: pdf.onError,
+      onPageError: pdf.onPageError,
+      gestureRecognizers: pdf.gestureRecognizers,
+      enableSwipe: pdf.enableSwipe,
+      swipeHorizontal: pdf.swipeHorizontal,
+      password: pdf.password,
+      nightMode: pdf.nightMode,
+      autoSpacing: pdf.autoSpacing,
+      pageFling: pdf.pageFling,
+      pageSnap: pdf.pageSnap,
+      fitEachPage: pdf.fitEachPage,
+      defaultPage: pdf.defaultPage,
+      fitPolicy: pdf.fitPolicy,
+      preventLinkNavigation: pdf.preventLinkNavigation,
     );
   }
 }
